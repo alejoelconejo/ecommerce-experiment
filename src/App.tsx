625c08de878ca6ec21db819fa08b22a5244bc67b
Alejo Janin <alejojanin@gmail.com>
2022-10-18 18:04:26 -0300

    Refactor Products component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from 'react'
 import api from './api'
 import Header from './Header'
+import Products from './Products'
 import Spinner from './Spinnert'
-
-export interface Rating {
-    rate: number
-    count: number
-}
 
 export interface Product {
     id: number
@@ -18,8 +14,9 @@ export interface Product {
     rating: Rating
 }
 
-interface getRanking {
-    (ranking: number): string
+export interface Rating {
+    rate: number
+    count: number
 }
 
 function App() {
@@ -33,59 +30,20 @@ function App() {
         })
     }, [])
 
-    const getRanking: getRanking = (ranking) => {
-        return '★'.repeat(ranking).padEnd(5, '☆')
-    }
-
     return (
         <div className='bg-slate-800 text-gray-200 min-h-screen'>
             <Header />
             <main className='flex flex-col gap-8 mt-6 items-center'>
-                <h1 className='text-3xl font-bold'>
+                <h1 className='text-5xl font-bold'>
                     Welcome to
                     <span className='text-pink-300 pl-2'>Arturo Shop!</span>
                 </h1>
                 <p>The place where you ENJOY buying random stuff</p>
                 <section>
-                    <h2 className='text-2xl uppercase font-bold mb-4'>
+                    <h2 className='text-2xl uppercase font-bold mb-6'>
                         Products
                     </h2>
-                    {isLoading ? (
-                        <Spinner />
-                    ) : (
-                        <div>
-                            <ul className='grid grid-cols-cards gap-4 text-slate-800'>
-                                {products.map(
-                                    ({ title, id, image, price, rating }) => (
-                                        <li
-                                            key={id}
-                                            className='bg-white p-4 flex flex-col gap-4 rounded-md'
-                                        >
-                                            <img
-                                                src={image}
-                                                className='h-64 w-64 object-contain'
-                                            />
-                                            <footer className='flex flex-col'>
-                                                <p>
-                                                    {price.toLocaleString(
-                                                        'es-AR',
-                                                        {
-                                                            style: 'currency',
-                                                            currency: 'ARS',
-                                                        }
-                                                    )}
-                                                </p>
-                                                <h3>{title}</h3>
-                                                <p className='text-yellow-500'>
-                                                    {getRanking(rating.rate)}
-                                                </p>
-                                            </footer>
-                                        </li>
-                                    )
-                                )}
-                            </ul>
-                        </div>
-                    )}
+                    {isLoading ? <Spinner /> : <Products products={products} />}
                 </section>
             </main>
             <footer>footer</footer>
---
+++ src/Products.tsx
@@ -0,0 +1,39 @@
+import { Product } from './App'
+
+interface getRanking {
+    (ranking: number): string
+}
+const getRanking: getRanking = (ranking) => {
+    return '★'.repeat(ranking).padEnd(5, '☆')
+}
+
+const Products = ({ products }: { products: Product[] }) => {
+    return (
+        <div>
+            <ul className='grid grid-cols-cards gap-4 text-slate-800'>
+                {products.map(({ title, id, image, price, rating }) => (
+                    <li
+                        key={id}
+                        className='bg-white p-4 flex flex-col gap-4 rounded-md'
+                    >
+                        <img src={image} className='h-64 w-64 object-contain' />
+                        <footer className='flex flex-col'>
+                            <p>
+                                {price.toLocaleString('es-AR', {
+                                    style: 'currency',
+                                    currency: 'ARS',
+                                })}
+                            </p>
+                            <h3>{title}</h3>
+                            <p className='text-yellow-500'>
+                                {getRanking(rating.rate)}
+                            </p>
+                        </footer>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Products
