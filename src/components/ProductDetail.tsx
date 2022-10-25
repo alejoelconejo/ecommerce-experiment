5ebc4142920af2686d1456c5b407b46739e1b769
Alejo Janin <alejojanin@gmail.com>
2022-10-25 14:45:32 -0300

    Add handleClick to Add to cart button

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -6,6 +6,10 @@ interface Params {
 }
 
 export const ProductDetail = ({ productDetail }: Params) => {
+  const handleClick = (id: number) => {
+    console.log('added product ', id)
+  }
+
   return (
     <article className='flex gap-8 mb-12'>
       <figure className='p-8 bg-white rounded-md border-gray-100 border-2 flex justify-center items-center flex-1'>
@@ -24,7 +28,10 @@ export const ProductDetail = ({ productDetail }: Params) => {
           <p>{getRanking(productDetail.rating.rate)}</p>
           <span>({productDetail.rating.count})</span>
         </div>
-        <button className='bg-pink-500 hover:bg-pink-600 py-2 px-4 rounded-full text-gray-100 font-bold'>
+        <button
+          onClick={() => handleClick(productDetail.id)}
+          className='bg-pink-500 hover:bg-pink-600 py-2 px-4 rounded-full text-gray-100 font-bold'
+        >
           Add to Cart
         </button>
       </div>
