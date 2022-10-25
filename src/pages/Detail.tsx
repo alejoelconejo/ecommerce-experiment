e40abfa8468ad6e89b8ccaff7e440b0610642151
Alejo Janin <alejojanin@gmail.com>
2022-10-25 10:51:23 -0300

    Modify detail page styles

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -12,18 +12,27 @@ export const Detail = ({ products, params }: Params) => {
   const productDetail = products.filter((product) => product.id === paramsId)[0]
 
   return (
-    <section>
-      <img className='h-96' src={productDetail.image} />
-      <h2>{productDetail.title}</h2>
-      <p>{productDetail.description}</p>
-      <span>
-        {productDetail.price.toLocaleString('es-AR', {
-          style: 'currency',
-          currency: 'ARS',
-        })}
-      </span>
-      <p>{getRanking(productDetail.rating.rate)}</p>
-      <span>({productDetail.rating.count})</span>
+    <section className='flex max-w-4xl gap-8'>
+      <div className='p-8 bg-white rounded-md border-gray-100 border-2 flex justify-center items-center flex-1'>
+        <img className='w-auto h-64 object-contain' src={productDetail.image} />
+      </div>
+      <div className='flex-1'>
+        <h2 className='font-bold text-3xl mb-2'>{productDetail.title}</h2>
+        <p className='mb-8'>{productDetail.description}</p>
+        <span className='font-bold text-xl'>
+          {productDetail.price.toLocaleString('es-AR', {
+            style: 'currency',
+            currency: 'ARS',
+          })}
+        </span>
+        <div className='flex gap-1 mb-4'>
+          <p>{getRanking(productDetail.rating.rate)}</p>
+          <span>({productDetail.rating.count})</span>
+        </div>
+        <button className='bg-pink-500 hover:bg-pink-600 py-2 px-4 rounded-full text-gray-100 font-bold'>
+          Add to Cart
+        </button>
+      </div>
     </section>
   )
 }
