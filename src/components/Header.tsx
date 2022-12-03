207bf9d7e0e9ce258ae47d2b8a2bc50fe8016267
Alejo Janin <alejojanin@gmail.com>
2022-12-03 08:06:29 -0300

    Add styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
-import { SetStateAction, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'wouter'
 import Modal from 'react-modal'
+
 import { setTheme } from '../utils/setTheme'
 import { toggleTheme } from '../utils/toggleTheme'
-import darkIcon from '../assets/images/dark-icon.svg'
-import lightIcon from '../assets/images/light-icon.svg'
 import { Product } from '../types'
 import { Cart } from './Cart'
 import { useShoppingCart } from '../contexts/CartContext'
+
+import darkIcon from '../assets/images/dark-icon.svg'
+import lightIcon from '../assets/images/light-icon.svg'
+import cartIcon from '../assets/images/cart.svg'
 
 interface Props {
   products: Product[]
@@ -37,38 +40,40 @@ const Header = ({ products }: Props) => {
           El Conejo Shop
         </h1>
       </Link>
-      <nav>
-        <ul className='flex gap-8'>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Electronics</li>
-          <li>Jewelry</li>
-        </ul>
-      </nav>
-      <div>
+      <div className='flex gap-8 items-center'>
+        <div>
+          <button
+            id='theme-toggle'
+            aria-label='Toggle Theme'
+            type='button'
+            onClick={handleClickDark}
+            className='text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5'
+          >
+            <img
+              src={darkIcon}
+              alt='dark-icon'
+              id='theme-toggle-dark-icon'
+              className='hidden w-5 h-5'
+            />
+            <img
+              src={lightIcon}
+              alt='light-icon'
+              id='theme-toggle-light-icon'
+              className='hidden w-5 h-5'
+            />
+          </button>
+        </div>
         <button
-          id='theme-toggle'
-          aria-label='Toggle Theme'
-          type='button'
-          onClick={handleClickDark}
-          className='text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none rounded-lg text-sm p-2.5'
+          onClick={handleClickCart}
+          className='bg-purple-600 hover:bg-purple-500 transition-colors duration-75 rounded-full p-3 relative'
         >
-          <img
-            src={darkIcon}
-            alt='dark-icon'
-            id='theme-toggle-dark-icon'
-            className='hidden w-5 h-5'
-          />
-          <img
-            src={lightIcon}
-            alt='light-icon'
-            id='theme-toggle-light-icon'
-            className='hidden w-5 h-5'
-          />
+          <img src={cartIcon} className='h-6 w-6' />
+          <span className='absolute bg-gray-100 dark:bg-gray-800 rounded-full border-purple-600 border-2 bottom-[-11px] right-[-11px] px-2'>
+            {cartQuantity}
+          </span>
         </button>
       </div>
-      <button onClick={handleClickCart}>Cart</button>
-      <span>{cartQuantity}</span>
+
       <Modal
         isOpen={isModalOpen}
         onRequestClose={() => toggleModalOpen(false)}
