43e6096db7a69c51d18d8ab4f854425ca30ec9a8
Alejo Janin <alejojanin@gmail.com>
2022-12-06 07:32:48 -0300

    Add feat to increase product quantity

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,4 +1,3 @@
-import { SetStateAction } from 'react'
 import { getRanking } from '../utils/getRanking'
 import { Product } from '../types'
 import { useShoppingCart } from '../contexts/CartContext'
@@ -15,6 +14,8 @@ export const ProductDetail = ({ productDetail }: Props) => {
     removeFromCart,
   } = useShoppingCart()
 
+  const quantity = getItemQuantity(productDetail.id)
+
   return (
     <article className='flex gap-8 mb-12'>
       <figure className='p-8 bg-white rounded-md border-gray-100 border-2 flex justify-center items-center flex-1'>
@@ -24,25 +25,56 @@ export const ProductDetail = ({ productDetail }: Props) => {
           alt={productDetail.title}
         />
       </figure>
-      <div className='flex-1'>
-        <h2 className='font-bold text-3xl mb-2'>{productDetail.title}</h2>
-        <p className='mb-8'>{productDetail.description}</p>
-        <span className='font-bold text-xl'>
-          {productDetail.price.toLocaleString('es-AR', {
-            style: 'currency',
-            currency: 'ARS',
-          })}
-        </span>
-        <div className='flex gap-1 mb-4'>
-          <p>{getRanking(productDetail.rating.rate)}</p>
-          <span>({productDetail.rating.count})</span>
+      <div className='flex-1 flex-col justify-between flex'>
+        <div>
+          <h2 className='font-bold text-3xl mb-2'>{productDetail.title}</h2>
+          <p className='mb-8'>{productDetail.description}</p>
+          <span className='font-bold text-xl'>
+            {productDetail.price.toLocaleString('es-AR', {
+              style: 'currency',
+              currency: 'ARS',
+            })}
+          </span>
+          <div className='flex gap-1 mb-4'>
+            <p>{getRanking(productDetail.rating.rate)}</p>
+            <span>({productDetail.rating.count})</span>
+          </div>
         </div>
-        <button
-          onClick={() => increaseCartQuantity(productDetail.id)}
-          className='bg-pink-500 hover:bg-pink-600 py-2 px-4 rounded-full text-gray-100 font-bold'
-        >
-          Add to Cart
-        </button>
+
+        <div className=''>
+          {quantity === 0 ? (
+            <button
+              onClick={() => increaseCartQuantity(productDetail.id)}
+              className='bg-pink-500 hover:bg-pink-600 py-2 px-4 rounded-full text-gray-100 font-bold'
+            >
+              Add to Cart
+            </button>
+          ) : (
+            <div className='flex gap-4'>
+              <div className='flex gap-4 items-center'>
+                <button
+                  className='text-3xl'
+                  onClick={() => decreaseCartQuantity(productDetail.id)}
+                >
+                  -
+                </button>
+                <span className='text-xl'>{quantity}</span>
+                <button
+                  className='text-3xl'
+                  onClick={() => increaseCartQuantity(productDetail.id)}
+                >
+                  +
+                </button>
+              </div>
+              <button
+                className='px-4 py-2 rounded-full bg-gray-300 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600'
+                onClick={() => removeFromCart(productDetail.id)}
+              >
+                Remove
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </article>
   )
