68540c304f1c64fc2e7e839a9d4d024c9c53b473
Alejo Janin <alejojanin@gmail.com>
2022-12-08 07:00:57 -0300

    Fix import

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
-import { Product } from './App'
+import { Product } from './types'
 
 const apiUrl: string = 'https://fakestoreapi.com/products'
 
 export default async function api(): Promise<Product[]> {
-    return fetch(apiUrl).then((res) => res.json())
+  return fetch(apiUrl).then((res) => res.json())
 }
