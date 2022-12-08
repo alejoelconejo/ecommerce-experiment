2ce1353521e0590433b5e542938b1d87b593c032
Alejo Janin <alejojanin@gmail.com>
2022-12-08 07:08:02 -0300

    Add alt to imgs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const Header = ({ products }: Props) => {
   return (
     <header className='flex justify-between py-4 items-center max-w-7xl mx-auto mb-4'>
       <Link href='/' className='flex gap-4 items-center'>
-        <img src={logo} className='w-12 h-12' />
+        <img src={logo} alt='logo' className='w-12 h-12' />
         <h1 className='text-3xl bg-gradient-to-r from-[#E67D4B] to-[#E6C14B] bg-clip-text text-transparent font-bold cursor-pointer md:block hidden'>
           El Conejo Shop
         </h1>
@@ -47,7 +47,7 @@ const Header = ({ products }: Props) => {
           onClick={handleToggleDrawer}
           className='bg-purple-600 hover:bg-purple-500 transition-colors duration-75 rounded-full p-3 relative'
         >
-          <img src={cartIcon} className='h-6 w-6' />
+          <img src={cartIcon} alt='cart' className='h-6 w-6' />
           <span className='absolute bg-gray-100 dark:bg-gray-800 rounded-full border-purple-600 border-2 bottom-[-11px] right-[-11px] px-2'>
             {cartQuantity}
           </span>
